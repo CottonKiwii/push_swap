0375d97e960657d03ec9ceb710aed5918ab2db3f
Julia Wolfram <julia.wolfram@gmx.at>
2024-06-19 18:16:18 +0200

    refactor: added ft_split in case of input being one string

--- src/push_swap/main.c
+++ src/push_swap/main.c
@@ -6,7 +6,7 @@
 /*   By: jwolfram <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/18 13:40:23 by jwolfram          #+#    #+#             */
-/*   Updated: 2024/06/18 17:46:06 by jwolfram         ###   ########.fr       */
+/*   Updated: 2024/06/19 18:14:45 by jwolfram         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,9 @@ int	main(int ac, char **av)
 	if (ac == 1)
 		return (0);
 	i = 0;
+	if (ac < 3)
+	{
+		av = ft_split(av[1], ' ');
+	}
 	a->content = ft_lstnew(ft_atoi(av[1]));
 }
