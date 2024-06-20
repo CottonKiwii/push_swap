0b3844c550bbacf2dfda54a1b5eaaf021cddab60
Julia Wolfram <julia.wolfram@gmx.at>
2024-06-20 18:15:49 +0200

    refactor: love of my life added cute main

--- src/main.c
+++ src/main.c
@@ -1,26 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jwolfram <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/02 17:36:03 by jwolfram          #+#    #+#             */
-/*   Updated: 2024/04/04 17:38:43 by jwolfram         ###   ########.fr       */
+/*   Created: 2024/06/18 13:40:23 by jwolfram          #+#    #+#             */
+/*   Updated: 2024/06/20 18:14:11 by jwolfram         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+/*#include "libft.h"
+#include "push_swap.h"
 
-void	*ft_memset(void *s, int c, size_t n)
+int	main(int ac, char **av)
 {
-	size_t	i;
+	t_list	*a;
+	t_list	*b;
+	int		i;
 
+	if (ac == 1)
+		return (0);
 	i = 0;
-	while (i < n)
+	if (ac < 3)
 	{
-		*(char *)(s + i) = c;
-		i++;
+		av = ft_split(av[1], ' ');
 	}
-	return (s);
+	a->content = ft_lstnew(ft_atoi(av[1]));
+}*/
+
+#include "ft_printf.h"
+
+int main(int argc, char **argv)
+{
+	(void)argc;
+	(void)argv;
+	char *love_of_my_life = "Julia";
+	ft_printf("I love %s<3", love_of_my_life);
 }
