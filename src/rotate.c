46de3c6bbdc39a58b886e2ef8b2f0707e87d631d
CottonKiwii <julia.wolfram@gmx.at>
2024-06-28 17:16:29 +0200

    feat: working on rotation and testing linked list setup

--- src/rotate.c
+++ src/rotate.c
@@ -4,15 +4,7 @@
 
 int	rotate_a(t_type **a)
 {
-	t_type *last;
-	t_type *temp;
-
 	if (!*a || !(*a)->next)
 		return (ERR);
-	last = *a;
-	temp = last;
-	(*a)->next = temp->next;
-	temp->next = *a;
-	*a = temp;
 	return (ft_printf("ra\n"), SUCC);
 }
--- src/tester.c
+++ src/tester.c
@@ -6,19 +6,27 @@
 /*   By: CottonKiwii <julia.wolfram@gmx.at>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/26 17:39:44 by jwolfram          #+#    #+#             */
-/*   Updated: 2024/06/28 14:12:59 by CottonKiwii      ###   ########.fr       */
+/*   Updated: 2024/06/28 16:37:34 by CottonKiwii      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include "push_swap.h"
 
-void	tester(t_type *lst)
+void	tester(t_type **lst)
 {
-	while (lst)
+	while (*lst)
 	{
-		ft_printf("%d\n", lst->content);
-		lst = lst->next;
+		ft_printf("%d\n", (*lst)->content);
+		*lst = (*lst)->next;
 	}
+	ft_printf("\n");
+	*lst = (*lst)->prev;
+	while (*lst)
+	{
+		ft_printf("%d\n", (*lst)->content);
+		*lst = (*lst)->prev;
+	}
+//	ft_printf("%d\n", lst->last);
 }
 
