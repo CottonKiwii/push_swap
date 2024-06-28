1119bf1d44ad7394e74f158dca8fde31dd34ce01
CottonKiwii <julia.wolfram@gmx.at>
2024-06-28 14:18:34 +0200

    feat: added rest of swap and checked if it works

--- src/swap.c
+++ src/swap.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   swap.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jwolfram <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: CottonKiwii <julia.wolfram@gmx.at>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/27 17:43:19 by jwolfram          #+#    #+#             */
-/*   Updated: 2024/06/27 18:32:30 by jwolfram         ###   ########.fr       */
+/*   Updated: 2024/06/28 13:09:22 by CottonKiwii      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,36 @@ int	swap_a(t_type **a)
 	t_type	*temp;
 
 	if (!*a || !(*a)->next)
-		return(0);
+		return(ERR);
 	temp = (*a)->next;
 	(*a)->next = temp->next;
-	temp->next = (*a);
+	temp->next = *a;
 	*a = temp;
-	return (ft_printf("sa\n"), 1);
+	return (ft_printf("sb\n"), SUCC);
+}
+
+int	swap_b(t_type **b)
+{
+	t_type	*temp;
+
+	if (!*b || !(*b)->next)
+		return(ERR);
+	temp = (*b)->next;
+	(*b)->next = temp->next;
+	temp->next = *b;
+	*b = temp;
+	return (ft_printf("sb\n"), SUCC);
+}
+
+int	swap_swap(t_type **a, t_type **b)
+{
+	int	i;
+
+	i = swap_a(a);
+	if (i == ERR)
+		return (ERR);
+	i = swap_b(b);
+	if (i == ERR)
+		return (ERR);
+	return (SUCC);
 }
