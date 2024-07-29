efa0305f099562df16d0ccaaa7f3f344b4928c0f
Julia Wolfram <julia.wolfram@gmx.at>
2024-07-29 19:12:45 +0200

    refactor: started set_stack function

--- src/stack.c
+++ src/stack.c
@@ -6,7 +6,7 @@
 /*   By: CottonKiwii <julia.wolfram@gmx.at>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/19 11:34:59 by jwolfram          #+#    #+#             */
-/*   Updated: 2024/07/29 14:33:30 by jwolfram         ###   ########.fr       */
+/*   Updated: 2024/07/29 19:11:49 by jwolfram         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,29 +24,40 @@ void	stack_init(t_link *stack, char c)
 
 char	**ft_set_str(char **av, int check)
 {
-	int	i;
 	char **str;
 
-	i = 0;
 	if (check)
 		str = ft_split(av[1], ' ');
 	else
-		str = av;
+		str = &av[1];
 	return (str);
 }
 
-void	stack_feed(t_link *stack, int ac, char **av)
+ft_set_stack(t_link	*stack, char **str)
+{
+	t_node	*node;
+	int i;
+
+	i = 0;
+	node = stack->first;
+	while (str[i])
+	{
+		node = ft_allocate();
+	}
+}
+
+t_link	*stack_feed(t_link *stack, int ac, char **av)
 {
 	int		check;
 	char	**str;
 	
 	check = 0;
 	if (!av)
-		return (*stack);
+		return (stack);
 	str = av;
 	if (ac == 2)
 		check = 1;
-	str = ft_set_str();
+	str = ft_set_str(av, check);
 	if (!str)
 		ft_exit();
 	if (!ft_set_stack())
