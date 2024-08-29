07693a6df5f7b79e177789e5b202dbef48855621
Julia Wolfram <julia.wolfram@gmx.at>
2024-08-29 13:57:20 +0200

    refactor: moved some stuff into split_chunk function

--- src/sort.c
+++ src/sort.c
@@ -6,7 +6,7 @@
 /*   By: jwolfram <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/23 18:06:23 by jwolfram          #+#    #+#             */
-/*   Updated: 2024/08/29 12:35:50 by jwolfram         ###   ########.fr       */
+/*   Updated: 2024/08/29 13:56:09 by jwolfram         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,14 +38,12 @@ void set_count(t_link *stack, t_chunk chunk)
 {
 	t_node	*cur;
 	t_node	*comp;
-	int		len;
 
-	len = chunk.len;
 	if (chunk.loc == TOP_A || chunk.loc == TOP_B)
 		comp = stack->first;
 	else
 		comp = stack->last;
-	while (len > 0)
+	while (chunk.len > 0)
 	{
 		cur = stack->first;
 		comp->procsd = 0;
@@ -55,60 +53,55 @@ void set_count(t_link *stack, t_chunk chunk)
 				comp->procsd++;
 			cur = cur->next;
 		}
-		comp = comp->next;
-		len--;
+		if (chunk.loc == TOP_A || chunk.loc == TOP_B)
+			comp = comp->next;
+		else
+			comp = comp->prev;
+		chunk.len--;
+	}
+}
+
+void	split_chunk(t_link *a, t_link *b, t_split *split, t_chunk chunk)
+{
+	t_node	*comp;
+	int		big;
+	int		mid;
+
+	mid = chunk.len / 3;
+	big = mid * 2;
+	split_init(split);
+	while (chunk.len > 0)
+	{
+		comp = a->first;
+		if (comp->procsd >= big)
+		{
+			split->big.len++;
+			sent_to(a, b, BOTTOM_A);
+		}
+		else if (comp->procsd >= mid)
+		{
+			split->mid.len++;
+			sent_to(a, b, TOP_B);
+		}
+		else
+	{
+			split->min.len++;
+			sent_to(a, b, BOTTOM_B);
+		}
+		chunk.len--;
 	}
 }
 
 void	threeway_sort(t_link *stack_a, t_link *stack_b, t_chunk chunk)
 {
-	t_node	*cur;
-	t_node	*comp;	
 	t_split	split;
-	int		big;
-	int		mid;
-	int		count;
 
 	if (chunk.len == 1 || chunk.len == 2)
 		return ;
-	mid = chunk.len / 3;
-	big = mid * 2;
-	count = chunk.len;
-	comp = stack_a->first;
-	split_init(&split);
-	while (count > 0)
-	{
-		cur = stack_a->first;
-		comp->procsd = 0;
-		while (cur)
-		{
-			if (comp != cur && comp->content > cur->content)
-				comp->procsd++;
-			cur = cur->next;
-		}
-		comp = comp->next;
-		count--;
-	}
-	count = chunk.len;
-	while (count > 0)
-	{
-		comp = stack_a->first;
-		if (comp->procsd >= big)
-		{
-			split.big.len++;
-			sent_to(stack_a, stack_b, BOTTOM_A);
-		}
-		else if (comp->procsd >= mid)
-		{
-			split.mid.len++;
-			sent_to(stack_a, stack_b, TOP_B);
-		}
-		else
-		{
-			split.min.len++;
-			sent_to(stack_a, stack_b, BOTTOM_B);
-		}
-		count--;
-	}
+	if (chunk.loc == TOP_A || chunk.loc == BOTTOM_A)
+		set_count(stack_a, chunk);
+	else
+		set_count(stack_b, chunk);
+	split_chunk(stack_a, stack_b, &split, chunk);
 	threeway_sort(stack_a, stack_b, split.big);
 }
